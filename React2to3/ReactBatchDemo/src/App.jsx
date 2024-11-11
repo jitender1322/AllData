16228624adef8e0e13ca67b181a4912e846a042c
jitender1322 <jitendersarswat6@gmail.com>
2024-11-11 10:40:22 +0530

    practice

--- React2to3/ReactBatchDemo/src/App.jsx
+++ React2to3/ReactBatchDemo/src/App.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import { store } from './app/Store';
-import ReduxApi from './Redux/ReduxApi';
 
 export default function App() {
 
   return (
     <div>
-      <Provider store={store} >
-        <ReduxApi />
-      </Provider>
+      <h1>hello</h1>
     </div>
   )
 }
