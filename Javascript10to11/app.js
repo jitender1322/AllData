11038a2a9cf950e03681d55655388e6c77425c98
jitender1322 <jitendersarswat6@gmail.com>
2025-04-28 10:41:41 +0530

    practice

--- Javascript10to11/app.js
+++ Javascript10to11/app.js
@@ -4,9 +4,6 @@ let age = document.getElementById("age");
 let btn = document.getElementById("btn");
 let list = document.getElementById("list");
 
-
-
-
 btn.addEventListener("click", () => {
   let obj = {
     id: Date.now(),
@@ -21,32 +18,54 @@ btn.addEventListener("click", () => {
   subject.value = "";
   age.value = "";
 
-  getData()
+  getData();
 });
 
-function getData(){
+function getData() {
   let allData = JSON.parse(localStorage.getItem("Students"));
-  list.innerHTML = ""
-  
-  allData.forEach((item) => {
+  list.innerHTML = "";
+
+  allData.forEach((item, index) => {
     let li1 = document.createElement("li");
     let li2 = document.createElement("li");
     let li3 = document.createElement("li");
     let li4 = document.createElement("li");
-    let img = document.createElement("img");
+    let del = document.createElement("button");
+    let edit = document.createElement("button");
 
-    li1.innerText = item.id
-    li2.innerText = item.name
-    li3.innerText = item.subject
-    li4.innerText = item.age
+    del.setAttribute("data-id", item.id);
+    edit.setAttribute("data-id", item.id);
 
-    list.append(li1)
-    list.append(li2)
-    list.append(li3)
-    list.append(li4)
+    li1.innerText = index + 1;
+    li2.innerText = item.name;
+    li3.innerText = item.subject;
+    li4.innerText = item.age;
+    del.innerHTML = "Delete";
+    edit.innerHTML = "Edit";
+    del.classList = "delete-btn";
+    edit.classList = "edit-btn";
+
+    list.append(li1);
+    list.append(li2);
+    list.append(li3);
+    list.append(li4);
+    list.append(del);
+    list.append(edit);
   });
 
+  let deleteBtns = document.querySelectorAll(".delete-btn");
+
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let allData = JSON.parse(localStorage.getItem("Students"));
+      let btnId = btn.getAttribute("data-id");
+
+      let newRecord = allData.filter((item)=>item.id != btnId);
+
+      localStorage.setItem("Students",JSON.stringify(newRecord))
+      getData()
+    });
+  });
 }
 
-getData()
-
+getData();
